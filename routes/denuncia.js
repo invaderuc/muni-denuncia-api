03e8329254c132d4be02d9d560db06d987e57fee
Gerard Andrés Venegas Vargas <gerarduc@gmail.com>
2022-06-11 00:50:07 -0400

    fix:ruta controlador denucia

--- routes/denuncia.js
+++ routes/denuncia.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const { create, list, read, update, remove, removeSoft,listAll,denunciasCount } = require("../Controllers/denuncia");
+const { create, list, read, update, remove, removeSoft,listAll,denunciasCount } = require("../controllers/denuncia");
 
 // endpoints
 router.post("/denuncia", create);
@@ -8,7 +8,6 @@ router.get("/denuncia/:slug", read);
 router.put("/denuncia/:slug", update);
 router.patch("/denuncia/:slug", removeSoft);
 router.delete("/denuncia/:slug", remove); 
-
 router.get("/denuncias", listAll);
 router.get("/denuncias/total", denunciasCount);
 router.post("/denuncias", list);
